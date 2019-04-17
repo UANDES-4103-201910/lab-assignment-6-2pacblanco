a855aa601171c03a97c137f6e7d7e6b709fcb051
2pacblanco <jadonoso2@miuandes.cl>
2019-04-17 13:20:40 -0400

    registrations done

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,17 @@ class RegistrationsController < ApplicationController
 	end
 
 	def create
-	    #complete this method
+	    #done
+		@user = User.create(user_params)
+		if @user.save
+			redirect_to "/" , notice: 'Registro exitoso!'
+		else
+			redirect_to registrations_url, notice: @user.errors.messages
+		end  
+
+	end
+
+	def user_params
+		params.require(:registrations).permit(:name,:last_name,:email,:password,:phone)
 	end
 end
